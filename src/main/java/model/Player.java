7e49cea825a3c1c65eafa553c48016b483f16e0d
IlosvayAron <ilosvayaron@gmail.com>
2021-05-14 17:37:47 +0200

    Created the Player class

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,34 @@
+package model;
+
+import lombok.Data;
+
+
+@Data
+public class Player {
+
+    // Data Fields
+    private String name;
+    private int turnScore;
+    private int totalScore;
+
+    // Constructor
+    public Player(String name){
+        this.name = name;
+        turnScore = 0;
+        totalScore = 0;
+    }
+
+    // Game Play Methods
+    public void resetTurnScore(){
+        turnScore = 0;
+    }
+
+    public void updateTurn(int roll){
+        turnScore += roll;
+    }
+
+    public void saveScore(){
+        totalScore += turnScore;
+        resetTurnScore();
+    }
+}

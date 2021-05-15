e2507c78664a04e079a8fce0cf08287ff5d6d025
IlosvayAron <ilosvayaron@gmail.com>
2021-05-15 22:24:43 +0200

    The application Start Point was created

---
+++ src/main/java/pig/gui/Main.java
@@ -0,0 +1,10 @@
+package pig.gui;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(PigApp.class, args);
+    }
+}

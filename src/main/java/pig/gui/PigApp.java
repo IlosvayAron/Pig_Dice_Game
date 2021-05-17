e07804eb62036940cc9160bc3a68ffc59c2d3931
IlosvayAron <ilosvayaron@gmail.com>
2021-05-18 00:25:26 +0200

    Logging some information in PigApp

--- src/main/java/pig/gui/PigApp.java
+++ src/main/java/pig/gui/PigApp.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.tinylog.Logger;
 
 public class PigApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Logger.info("Elindult az alkalmazás!");
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/PigGUI.fxml"));
         stage.setTitle("Pig Dice Game");
         stage.setScene(new Scene(root));

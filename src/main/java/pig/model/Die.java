02bac38eb5afd00452c35cd6b7941fd5bac3a6b7
ilosvayaron <ilosvayaron@gmail.com>
2021-05-17 12:14:51 +0200

    Die class was provided with API documentation

--- src/main/java/pig/model/Die.java
+++ src/main/java/pig/model/Die.java
@@ -2,6 +2,9 @@ package pig.model;
 
 import lombok.Data;
 
+/**
+ * Ez az osztaly reprezentalja a kockat, amellyel jatszani fogunk.
+ * */
 @Data
 public class Die {
 
@@ -10,6 +13,9 @@ public class Die {
     private int top;
 
     // Constructors
+    /**
+     * Bealitjuk a sides es a top valtozo alapertekit.
+     * */
     public Die(){
         sides = 6;
         top = 0;
@@ -20,14 +26,19 @@ public class Die {
         this.top = top;
     }
 
-    // Mutator Methods
+    /**
+     * @param top beallitja ezen osztaly top valtozojat arra az ertekre amit a kockaval dobtunk,
+     * de csak akkor ha az ervenyes. Azaz 0 es 6 kozott van.
+     */
     public void setTop(int top){
         if (top > 0 && top <= sides){
             this.top = top;
         }
     }
 
-    // Game Play Methods
+    /**
+     * 0 és 6 kozott 'dobunk' egy szamot. Ez lesz a kockaval dobott aktualis ertek.
+     */
     public void roll(){
         top = 1 + (int)(Math.random() * sides);
     }

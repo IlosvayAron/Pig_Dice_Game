448a5e5bd23dbf57392ecc76be7b70336fe03cd7
ilosvayaron <ilosvayaron@gmail.com>
2021-05-17 12:15:22 +0200

    Player class was provided with API documentation

--- src/main/java/pig/model/Player.java
+++ src/main/java/pig/model/Player.java
@@ -2,7 +2,10 @@ package pig.model;
 
 import lombok.Data;
 
-
+/**
+ * Ez az osztály fogja azon informáciokat tárolni, hogy a játékosnak, mennyi pontja van a jelenlegi körben
+ * és mennyi van összesen.
+ */
 @Data
 public class Player {
 
@@ -11,22 +14,35 @@ public class Player {
     private int turnScore;
     private int totalScore;
 
-    // Constructor
+    /**
+     * @param name nevű játékosnak a játék kezdetekor 0 a turnScore és a totalScore értéke.
+     */
     public Player(String name){
         this.name = name;
         turnScore = 0;
         totalScore = 0;
     }
 
-    // Game Play Methods
+    /**
+     * Nullára allítjuk a turnScore értékét mert a játékos 1-es értéket dobott a kockával.
+     */
     public void resetTurnScore(){
         turnScore = 0;
     }
 
+    /**
+     *
+     * @param roll értéket hozzáadajuk minden dobást követően a turnScore-hoz ezzel figyelve, hogy a
+     * jatékosnak mennyi az aktuális pontja ebben a körben.
+     */
     public void updateTurn(int roll){
         turnScore += roll;
     }
 
+    /**
+     * Mentjük a totalScore értéket és nullázuk a turnScore értékét, mivel a következő játékosé
+     * a dobási lehetőség.
+     */
     public void saveScore(){
         totalScore += turnScore;
         resetTurnScore();

45fe504b0c0f768e95551a8b7d5819d14e5761a9
ilosvayaron <ilosvayaron@gmail.com>
2021-05-17 12:15:43 +0200

    Game class was provided with API documentation

--- src/main/java/pig/model/Game.java
+++ src/main/java/pig/model/Game.java
@@ -2,10 +2,13 @@ package pig.model;
 
 import lombok.Data;
 
+/**
+ * Ez az osztály reprezentálja a játék menetét.
+ */
 @Data
 public class Game {
 
-    public static final int MAX_SCORE = 100;
+    public static final int MAX_SCORE = 50;
 
     // Data Fields
     private Die die;
@@ -13,7 +16,12 @@ public class Game {
     private Player playerTwo;
     private Player currentPlayer;
 
-    // The start state
+    /**
+     * A jéték kezdetekor létrehozzuk a két játékos és a kocka példányt.
+     *
+     * @param playerOneName az első játékos példánya
+     * @param playerTwoName a második játékos példánya
+     */
     public Game(String playerOneName, String playerTwoName ){
         die = new Die();
         playerOne = new Player(playerOneName);
@@ -21,17 +29,25 @@ public class Game {
         currentPlayer = playerOne;
     }
 
-    // Status Methods
-    // Current player's score is greater than 100 which means the game is over
+    /**
+     * Ha a jelenlegi játékos pontja nagyobb mint 50 akkor vége a játéknak és ezzel Ő a nyertes.
+     */
     public boolean gameOver(){
         return currentPlayer.getTotalScore() >= MAX_SCORE;
     }
 
+    /**
+     * Ellenőrizük, hogy az aktuális kör az első játékosé-e.
+     *
+     * @return csak egy True vagy False logikai értékkel tér vissza.
+     */
     public boolean playerOneTurn(){
         return currentPlayer == playerOne;
     }
 
-    // Game Play Methods
+     /**
+     * Ha az első játékosé a kör akkor Ő a currentPlayer ellenkező esetben a második játékos.
+     */
      public void switchTurn(){
         if (playerOneTurn()) {
             currentPlayer = playerTwo;
@@ -40,7 +56,11 @@ public class Game {
         }
      }
 
-     public void roll(){
+    /**
+     * Ha 1-es dobot a kockával az aktuális játékos akkor nullázuk az eddig összegyűjtött turnScore értéket
+     * és a második játékos köre kezdődik.
+     */
+    public void roll(){
         die.roll();
         int top = die.getTop();
         currentPlayer.updateTurn(top);
@@ -50,7 +70,11 @@ public class Game {
         }
      }
 
-     public void hold(){
+    /**
+     * Ha úgy dönt az aktuális játékos, hogy nem dob hanem megtartja az eddigi turnScore erteket
+     * akkor az hozzáadjuk a totalScore-hoz és a következő játékosé a dobási lehetőség.
+     */
+    public void hold(){
         currentPlayer.saveScore();
         switchTurn();
         die.setTop(0);
